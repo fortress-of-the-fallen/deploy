753f486f7f780fc03589b5338ec1888d7ae8a1d4
Minh Khang <qscvdefb@gmail.com>
2025-10-20 09:37:59 +0700

    Add initial project configuration and setup files
    Introduces environment variables, Docker Compose setup, application settings, and scripts for managing services. Includes configuration for MongoDB, MinIO, Redis, Ngrok, and orchestrator service, enabling local development and deployment.

---
+++ down.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff down -d
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff pull
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff up -d
