1b589e0cae8659046e5aa507328950fadc5f7af6
Minh Khang <qscvdefb@gmail.com>
2025-10-20 09:44:24 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 sudo docker compose -f docker-compose.yaml --env-file .env.local -p fotff pull
-sudodocker compose -f docker-compose.yaml --env-file .env.local -p fotff up -d
+sudo docker compose -f docker-compose.yaml --env-file .env.local -p fotff up -d
