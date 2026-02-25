4d324d825c9faea63edeb13e0cc437de3b544bb1
MK <qscvdefb@gmail.com>
2026-02-25 22:17:30 +0700

    up

--- down.sh
+++ down.sh
@@ -1,1 +1,4 @@
-sudo docker compose -f docker-compose.yaml --env-file .env.local -p fotff down -d
+#!/usr/bin/env bash
+set -euo pipefail
+
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff down --remove-orphans
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-sudo docker compose -f docker-compose.yaml --env-file .env.local -p fotff pull
-sudo docker compose -f docker-compose.yaml --env-file .env.local -p fotff up -d
+#!/usr/bin/env bash
+set -euo pipefail
+
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff pull || true
+docker compose -f docker-compose.yaml --env-file .env.local -p fotff up -d --build
